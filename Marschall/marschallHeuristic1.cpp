03ecffc878f3cd35891ae912fe66aa8e55b892c2
Lucas B. Rocha <lucas.lb.rocha@gmail.com>
2021-10-04 22:25:32 -0400

    Atualizando a Heuristica 1
    Agora a heuristica faz o mapeamento preenchendo os gaps

--- Marschall/marschallHeuristic1.cpp
+++ Marschall/marschallHeuristic1.cpp
@@ -3,71 +3,74 @@
 #include <queue>
 #include <bits/stdc++.h>
 
-int main (void)
+string mapeamento(Hash h, pair<unordered_map<int, string>,SequenceGraph> grafoSequencias, string sequence, int k)
 {
-    string sequence = "AAGACGTGTAGACGTGA", nomeArquivo = "kmers4.txt";
-    // AAGACGTGTAGACGTGA
-    map<string, int> kmersInTheGraph;
+    cout << sequence << endl;
+    int posicao = 0;
+    string resposta = "";
     vector<int> posicoesValidas;
-    int k = 3;
-    //cin >> sequence;
-    //cin >> nomeArquivo;
-    //cin >> k;
-    Hash h(k);   
     Marschall m;
-    h.populateGraph(nomeArquivo, false);
 
     for (int i = 0; i < sequence.length() - (k - 1); i++)
     {
         if (h.contains(sequence.substr(i,k)))
         {
-            cout << sequence.substr(i,k) << endl;
             posicoesValidas.push_back(i);
         }
     }
 
-    auto grafoSequencias = h.dbgToSequenceGraph_1();
-    
-    // mapeamento 
-    cout << sequence << endl;
     if (posicoesValidas.size() > 0)
     {
         if (posicoesValidas[0] != 0)
         {
-            cout << "(-," << posicoesValidas[0] << ") ";
+            //cout << "(-," << posicoesValidas[0] << ") ";
             string sequence_aux = sequence.substr(0, k + posicoesValidas[0]);
-            cout << "vamos procurar " << sequence_aux << endl;
             auto grafo_multicamada = m.buildMultilayerGraph(grafoSequencias.second, sequence_aux);
             auto retorno = m.dijkstra(grafo_multicamada, m.getInitialNode(), m.getEndNode());
             auto mapeamento = m.mostraMapeamento(retorno.first, grafoSequencias.first, grafoSequencias.second);    
-            cout << mapeamento << " com custo " << retorno.second << endl;     
+            resposta = mapeamento.substr(0,posicoesValidas[0]) + sequence.substr(posicoesValidas[0], k); 
+            posicao++;
         }
 
         for(int i = 0; i < posicoesValidas.size() - 1; i++)
         {     
-            cout << "(" << posicoesValidas[i] << "," << posicoesValidas[i + 1] << ") "; 
+            //cout << "(" << posicoesValidas[i] << "," << posicoesValidas[i + 1] << ") "; 
             int dif = posicoesValidas[i + 1] -  posicoesValidas[i]; 
             if (dif > 1)
             {
                 string sequence_aux = sequence.substr(posicoesValidas[i], k + dif);
-                cout << "vamos procurar " << sequence_aux << endl;
                 auto grafo_multicamada = m.buildMultilayerGraph(grafoSequencias.second, sequence_aux);
                 auto retorno = m.dijkstra(grafo_multicamada, m.getInitialNode(), m.getEndNode());
                 auto mapeamento = m.mostraMapeamento(retorno.first, grafoSequencias.first, grafoSequencias.second);    
-                cout << mapeamento << " com custo " << retorno.second << endl;            
+                if (mapeamento.length() >= k)      
+                {
+                    resposta = resposta + mapeamento.substr(k, mapeamento.length() - (2 * k));   
+                    //resposta = resposta + mapeamento.substr(mapeamento.length()- (k), k);   
+                    resposta = resposta + sequence.substr(posicoesValidas[i+1], k);                 
+                    posicao = 1;
+                }
+            }else{
+                if (posicao == 0)
+                    resposta = resposta + sequence.substr(posicoesValidas[i], k);
+                else
+                    resposta = resposta + sequence.substr(posicoesValidas[i] + (k), 1);
+                posicao++;
             }
         }
 
         if (posicoesValidas[posicoesValidas.size() - 1] != sequence.length() - k)
         {
-            cout << "(" << posicoesValidas[posicoesValidas.size() - 1] << ",-)" << endl;
+            //cout << "(" << posicoesValidas[posicoesValidas.size() - 1] << ",-)" << endl;
             int dif = sequence.length() - posicoesValidas[posicoesValidas.size() - 1];
-            string sequence_aux = sequence.substr(posicoesValidas[posicoesValidas.size() - 1], k + dif);
-            cout << "vamos procurar " << sequence_aux << endl;
-            auto grafo_multicamada = m.buildMultilayerGraph(grafoSequencias.second, sequence_aux);
-            auto retorno = m.dijkstra(grafo_multicamada, m.getInitialNode(), m.getEndNode());
-            auto mapeamento = m.mostraMapeamento(retorno.first, grafoSequencias.first, grafoSequencias.second);    
-            cout << mapeamento << " com custo " << retorno.second << endl;     
+            if (dif > 1)
+            {    
+                string sequence_aux = sequence.substr(posicoesValidas[posicoesValidas.size() - 1], k + dif);
+                auto grafo_multicamada = m.buildMultilayerGraph(grafoSequencias.second, sequence_aux);
+                auto retorno = m.dijkstra(grafo_multicamada, m.getInitialNode(), m.getEndNode());
+                auto mapeamento = m.mostraMapeamento(retorno.first, grafoSequencias.first, grafoSequencias.second);    
+                if (mapeamento.length() >= k)      
+                    resposta = resposta + mapeamento.substr(k, mapeamento.length() - k);  
+            }
         }
     } else
     {
@@ -75,9 +78,21 @@ int main (void)
         auto grafo_multicamada = m.buildMultilayerGraph(grafoSequencias.second, sequence);
         auto retorno = m.dijkstra(grafo_multicamada, m.getInitialNode(), m.getEndNode());
         auto mapeamento = m.mostraMapeamento(retorno.first, grafoSequencias.first, grafoSequencias.second);    
-        cout << mapeamento << " com custo " << retorno.second << endl;            
     }
+    return resposta;
+}
 
-
+int main (void)
+{
+    string sequence = "TTACGTAACGACG", nomeArquivo = "kmers4.txt";
+    // AAGACGTGTAGACGTGA
+    map<string, int> kmersInTheGraph;
+    int k = 3;
+    Hash h(k);   
+    h.populateGraph(nomeArquivo, false);
+    auto grafoSequencias = h.dbgToSequenceGraph_1();    
+    // mapeamento
+    auto retorno = mapeamento(h, grafoSequencias, sequence, k); 
+    cout << "Mapeamento " << retorno << endl;
     return 0;
 }
